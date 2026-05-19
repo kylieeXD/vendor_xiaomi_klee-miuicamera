bb493ccdd3afa08a5aa7d86f79b0fb82df921d65
kylieeXD <khayloaftck@gmail.com>
2026-05-19 12:46:54 +0000

    miuicamera: Extract camera using python extract utils
    Signed-off-by: kylieeXD <khayloaftck@gmail.com>

---
+++ klee-miuicamera-vendor.mk
@@ -0,0 +1,20 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+PRODUCT_SOONG_NAMESPACES += \
+    vendor/xiaomi/klee-miuicamera
+
+PRODUCT_COPY_FILES += \
+    vendor/xiaomi/klee-miuicamera/proprietary/system/etc/public.libraries-xiaomi.txt:$(TARGET_COPY_OUT_SYSTEM)/etc/public.libraries-xiaomi.txt
+
+PRODUCT_PACKAGES += \
+    librelight_only \
+    vendor.xiaomi.hardware.quickcamera-V1-ndk_platform \
+    libcamera_algoup_jni.xiaomi \
+    libcamera_ispinterface_jni.xiaomi \
+    libcamera_mianode_jni.xiaomi \
+    libmtkisp_metadata_sys \
+    vendor.mediatek.hardware.camera.isphal-V1-ndk-system_ext \
+    vendor.mediatek.hardware.camera.isphal@1.0-system_ext \
+    MiuiCamera
