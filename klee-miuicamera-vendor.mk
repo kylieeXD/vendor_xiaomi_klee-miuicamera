063fa5d79b9824ef0c5e79a65d213a4a7f3608a3
kylieeXD <khayloaftck@gmail.com>
2026-05-20 21:26:57 +0700

    miuicamera: Extract camera using python extract-utils
    Camera blobs from ruby, Liblaries from plato
    
    Signed-off-by: kylieeXD <khayloaftck@gmail.com>

---
+++ klee-miuicamera-vendor.mk
@@ -0,0 +1,15 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+PRODUCT_SOONG_NAMESPACES += \
+    vendor/xiaomi/klee-miuicamera
+
+PRODUCT_PACKAGES += \
+    libcamera_algoup_jni.xiaomi \
+    libcamera_ispinterface_jni.xiaomi \
+    libcamera_mianode_jni.xiaomi \
+    libmtkisp_metadata_sys \
+    vendor.mediatek.hardware.camera.isphal-V1-ndk-system_ext \
+    vendor.mediatek.hardware.camera.isphal@1.0-system_ext \
+    MiuiCamera
